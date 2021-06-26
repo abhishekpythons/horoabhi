12608d91541a43270a62315e88db103b84675632
unknown <abhishekpythons@gmail.com>
2021-06-26 11:52:41 +0530

    super user crested

--- horoscope/urls.py
+++ horoscope/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home),
-    path('horoscope/<slug:sign>/<slug:day>', views.horo)
+    path('horoscope/',views.chooseZodiac),
+    path('horoscope/<slug:sign>/<slug:day>', views.horo),
+    path('images/', views.imgs),
+    path('quotes/', views.quotes)
 ]
--- horoscope/views.py
+++ horoscope/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 import requests
-
+from django.contrib.auth.models import User
 
 signs=['aries', 'taurus', 'gemini', 'cancer', 'leo', 'virgo', 'libra', 'scorpio', 'sagitarus', 'capricorn', 'aquarius', 'pisces']
 dates=['21 Mar-19 Apr', '20 Apr-20 May', '21 May-20 Jun', 
@@ -11,10 +11,24 @@ color = ['lightblue', 'lightcoral', 'cyan', 'lightgreen', 'lightpink', 'lightsal
 # Create your views here.
 def home(request):
     tmp = {}
+    r = requests.post('https://zenquotes.io/api/random')
     for i in range(12):
         tmp[signs[i]] = {'color':color[i], 'date':{'from':dates[i].split('-')[0],'to':dates[i].split('-')[1]}}
-    context={'sign_dates': tmp}
-    return render(request, 'index.html', context=context)
+    context={'app':'home','sign_dates': tmp, 
+    'signs':signs,
+    'quote':eval(r._content)[0]}
+    return render(request,'index.html',context=context)
+
+def chooseZodiac(request):
+    if request.method == 'POST':
+        user = User.objects.create_user(request.POST['name'], 'lennon@thebeatles.com', 'johnpassword')
+
+    tmp = {}
+    for i in range(12):
+        tmp[signs[i]] = {'color':color[i], 'date':{'from':dates[i].split('-')[0],'to':dates[i].split('-')[1]}}
+    context={'app':'horoscope','sign_dates': tmp, 
+    'signs':signs,}
+    return render(request, 'chooseZodiac.html', context=context)
 
 def horo(request, sign='aries', day='today'):
     params = (
@@ -24,7 +38,7 @@ def horo(request, sign='aries', day='today'):
 
     r = requests.post('https://aztro.sameerkumar.website/', params=params)
     content = eval(r._content)
-    context={'sign': sign, 'day': day,
+    context={'app':'horoscope','sign': sign, 'day': day,
     'sign_dates':dict(zip(list(range(12)),dates)),
     'signs':signs,
     'current_date':content['current_date'],
@@ -40,3 +54,13 @@ def horo(request, sign='aries', day='today'):
     }
 
     return render(request, 'horoscope.html', context=context)
+
+def imgs(request):
+    return render(request, 'many.html', context={'app':'images','signs':signs})
+
+def quotes(request):
+    params=(('author','chanakya'),)
+    r = requests.post('https://zenquotes.io/api/random')
+    return render(request, 'many.html', context={'app':'quotes',
+    'signs':signs,
+    'quote':eval(r._content)[0]})
